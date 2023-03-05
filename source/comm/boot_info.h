86ce3d6916b9706299458891cda30f9d127352fe
JasonDnfkr <407405010@qq.com>
2023-03-05 23:04:09 +0800

    memory detect

---
+++ source/comm/boot_info.h
@@ -0,0 +1,17 @@
+#ifndef BOOT_INFO_H
+#define BOOT_INFO_H
+
+#include "types.h"
+
+#define BOOT_RAM_REGION_MAX     10
+
+typedef struct _boot_info_t {
+    struct {
+        uint32_t start;     // 起始地址
+        uint32_t size;      // 大小
+    } ram_region_cfg[BOOT_RAM_REGION_MAX];
+
+    int ram_region_count;   // 指示ram_region_cfg多少个数组是有效的
+} boot_into_t;
+
+#endif
