1c34df1e72b1ab2bac678443eaf592b0fe446b4b
JasonDnfkr <407405010@qq.com>
2023-03-05 22:21:57 +0800

    show a string msg

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -5,7 +5,19 @@
 
 __asm__(".code16gcc");
 
+static void show_msg(const char* msg) {
+    char c;
+    while ((c = *msg++) != '\0') {
+        __asm__ __volatile__(
+            "mov $0xe, %%ah\n\t"
+            "mov %[ch], %%al\n\t"
+            "int $0x10"::[ch]"r"(c)
+        );
+    }
+}
+
 void loader_entry(void) {
+    show_msg("....loading....\n\r");
     while (1) { }
 }
 
