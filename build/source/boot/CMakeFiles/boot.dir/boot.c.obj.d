2d636c5bb6fd3eed5c41d3345d55a27630a1db02
JasonDnfkr <407405010@qq.com>
2023-03-10 11:34:35 +0800

    add .gitignore

---
+++ build/source/boot/CMakeFiles/boot.dir/boot.c.obj.d
@@ -0,0 +1,3 @@
+source/boot/CMakeFiles/boot.dir/boot.c.obj: \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/boot/boot.c \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/boot/boot.h
---
+++ build/source/loader/CMakeFiles/loader.dir/loader_16.c.obj.d
@@ -0,0 +1,7 @@
+source/loader/CMakeFiles/loader.dir/loader_16.c.obj: \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/loader/loader_16.c \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/loader/loader.h \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/comm/boot_info.h \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/comm/types.h \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/comm/types.h \
+ E:/Data/Work/Job/Cpp/diy-x86os/start/start/source/comm/cpu_instr.h
