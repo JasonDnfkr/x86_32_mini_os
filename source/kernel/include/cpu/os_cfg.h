8ebfad9649af5b5b78906e2bd8e0eb359e7d3ea7
JasonDnfkr <407405010@qq.com>
2023-03-11 19:10:32 +0800

    timer: sys_tick++

--- source/kernel/include/cpu/os_cfg.h
+++ source/kernel/include/cpu/os_cfg.h
@@ -6,6 +6,9 @@
 #define KERNEL_SELECTOR_CS  (1 * 8)
 #define KERNEL_SELECTOR_DS  (2 * 8)
 
-#define KERNEL_STACK_SIZE (8 * 1024)    // 栈大小
+#define KERNEL_STACK_SIZE   (8 * 1024)    // 栈大小
+
+// 时钟中断周期
+#define OS_TICKS_MS         10
 
 #endif
