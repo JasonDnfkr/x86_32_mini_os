7795f3eeb003e7e3682df98de54f51ed2cf04cc6
JasonDnfkr <407405010@qq.com>
2023-03-19 00:02:30 +0800

    irq_protection: log_printf

--- source/comm/cpu_instr.h
+++ source/comm/cpu_instr.h
@@ -101,4 +101,21 @@ static inline void write_tr(uint16_t tss_sel) {
     __asm__ __volatile__("ltr %%ax"::"a"(tss_sel));
 }
 
+// read_eflags
+static inline uint32_t read_eflags(void) {
+    uint32_t eflags;
+    __asm__ __volatile__("pushf\n\tpop %%eax":"=a"(eflags));
+    return eflags;
+}
+
+
+// write_eflags
+static inline void write_eflags(uint32_t eflags) {
+    __asm__ __volatile__("push %%eax\n\tpopf"::"a"(eflags));
+}
+
+
 #endif
+
+
+
